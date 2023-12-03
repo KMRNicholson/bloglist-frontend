066a4fa28a01478fe12312c12180216c118166be
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-12-03 19:02:28 +0200

    refact[7.21]: replace Menu with NavigationBar in Dashboard component

--- src/components/home/Dashboard.jsx
+++ src/components/home/Dashboard.jsx
@@ -1,46 +1,20 @@
-import { useDispatch, useSelector } from "react-redux";
-import { Routes, Route, Link, useNavigate, useMatch } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Routes, Route, useMatch } from "react-router-dom";
+import { Container } from "@mui/material";
 import _ from "lodash";
-import { logout } from "../auth/reducers/authReducer";
 import BlogList from "../blogs/BlogList";
 import Blog from "../blogs/Blog";
 import UserList from "../users/UserList";
 import User from "../users/User";
 import Notification from "../shared/Notification";
-
-const Menu = ({ user }) => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    dispatch(logout());
-    navigate("/");
-  };
-
-  const padding = {
-    paddingRight: 5,
-  };
-
-  return (
-    <div>
-      <Link to="/blogs" style={padding}>
-        blogs
-      </Link>
-      <Link to="/users" style={padding}>
-        users
-      </Link>
-      <text style={padding}>{user.name}</text>
-      <button style={padding} onClick={handleLogout}>
-        logout
-      </button>
-    </div>
-  );
-};
+import NavigationBar from "../shared/NavigationBar";
 
 const Dashboard = () => {
   const auth = useSelector((state) => state.auth);
-  const blogs = useSelector((state) => _.orderBy(state.blogs, "likes", "desc"));
+  const unorderdBlogs = useSelector((state) => state.blogs);
   const users = useSelector((state) => state.users);
+
+  const blogs = _.orderBy(unorderdBlogs, "likes", "desc");
 
   const matchObjectByRoute = (route, list) => {
     const match = useMatch(route);
@@ -53,17 +27,18 @@ const Dashboard = () => {
   if (auth === null) return;
 
   return (
-    <div>
-      <Menu user={auth.user} />
+    <Container>
+      <NavigationBar user={auth.user} />
       <Notification />
 
       <Routes>
+        <Route path="/" element={<div />} />
         <Route path="/blogs" element={<BlogList blogs={blogs} />} />
         <Route path="/blogs/:id" element={<Blog blog={matchedBlog} />} />
         <Route path="/users" element={<UserList users={users} />} />
         <Route path="/users/:id" element={<User user={matchedUser} />} />
       </Routes>
-    </div>
+    </Container>
   );
 };
 
